938c4a89d3a6f110818f51ca1b9739848ba34d3a
Arturs <suhomiro.art@gmail.com>
2022-04-05 23:32:25 +0300

    first commit

--- app/src/main/java/test/contact_number/utils/Animation.kt
+++ app/src/main/java/test/contact_number/utils/Animation.kt
@@ -7,7 +7,7 @@ import android.view.View
 
 fun fadeInAnimation(viewToFadeIn: View) {
     val fadeIn = ObjectAnimator.ofFloat(viewToFadeIn, "alpha", 0f, 1f)
-    fadeIn.duration = 500
+    fadeIn.duration = 1000
     fadeIn.addListener(object : AnimatorListenerAdapter() {
         override fun onAnimationStart(animation: Animator?) {
             viewToFadeIn.visibility = View.VISIBLE
@@ -19,7 +19,7 @@ fun fadeInAnimation(viewToFadeIn: View) {
 
 fun fadeOutAnimation(viewToFadeOut: View) {
     val fadeOut = ObjectAnimator.ofFloat(viewToFadeOut, "alpha", 1f, 0f)
-    fadeOut.duration = 500
+    fadeOut.duration = 1000
     fadeOut.addListener(object : AnimatorListenerAdapter() {
         override fun onAnimationEnd(animation: Animator) {
             viewToFadeOut.visibility = View.GONE
